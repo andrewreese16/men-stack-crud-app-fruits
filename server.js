b8b3ea44d31a001ecb7dfe92c0510fd89d7a76f4
andrewr1616 <andrewreese1616@gmail.com>
2024-09-06 15:42:01 -0400

    finished up to rendering data to a page

--- server.js
+++ server.js
@@ -13,6 +13,45 @@ mongoose.connection.on("connected", function () {
   console.log(`Connected to MongoDB ${mongoose.connection.name}`);
 });
 
+// Middleware
+// this middleware parases the form requests
+// and lets us access req.body
+app.use(express.urlencoded({ extended: false }));
+
+// In order to create a Fruit
+
+// the client needs to make a request to get a form
+// GET request
+app.get("/fruits/new", async function (req, res) {
+  res.render("fruits/new.ejs");
+});
+
+app.get("/fruits", async function (req, res) {
+  const allFruitDocs = await FruitModel.find({});
+  console.log(allFruitDocs);
+
+  // secondd argument to render is an object
+  // we choose the key and value
+  // keyname will become variable name inside of the list argument ejs page
+  res.render("fruits/index.ejs", { fruitDocs: allFruitDocs });
+});
+
+// then the client needs to submit the form with the data representing the new fruit
+// POST request
+app.post("/fruits", async function (req, res) {
+  console.log(req.body, "<- contents of the form");
+  // take the contents of the form and put them in the database
+  if (req.body.isReadyToEat === "on") {
+    req.body.isReadyToEat = true;
+  } else {
+    req.body.isReadyToEat = false;
+  }
+
+  // use our model to add it to the database!
+  const createdFruit = await FruitModel.create(req.body);
+  console.log(createdFruit);
+  res.redirect("/fruits/new"); // we always redirect to the route we want the user to see
+});
 // ENDPOINTS!!!
 // Landing Page aka Root Route
 app.get("/", function (req, res) {
